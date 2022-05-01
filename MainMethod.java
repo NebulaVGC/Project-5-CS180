7fe5f9822140d19f9c2c135150774c7aff196d86
vickyd123 <102775884+vickyd123@users.noreply.github.com>
2022-05-01 18:11:53 -0400

    Update MainMethod.java

--- MainMethod.java
+++ MainMethod.java
@@ -6,40 +6,40 @@ import java.net.Socket;
 import java.util.Scanner;
 
 /**
- * Project 04 -- MainMethod
+ * Project 05 -- MainMethod
  * <p>
  * Extends the run method from login to organize which class is called
  *
  * @author Tim Chou L09
- * @version April 11, 2022
+ * @version May 1,2022
  */
 
 public class MainMethod extends Run {
     public static void main(String[] args) {
         int i = 0;
 
-            try {
-                    Socket socket = new Socket("localhost", 4242);
-                    String username = runQuiz(socket);
-                    if (username.equals("end")) {
-                        i++;
-                    } else if (username.equals("")) {
-                    } else {
-                        String[] splited = username.split(" ");
-                        if (splited[0].equals("Teacher")) {
-                            Teacher teacher = new Teacher(splited[1]);
-                            teacher.createMenuGUI(socket);
-                            i++;
-                        } else if (splited[0].equals("Student")) {
-                            Student student = new Student(splited[1], socket);
-                            student.mainStudent(socket);
-                            i++;
-                        }
-                    }
+        try {
+            Socket socket = new Socket("localhost", 4242); //SOCKET CONNECTION
+            String username = runQuiz(socket); //USER CONNECTS
+            if (username.equals("end")) {
+                i++;
+            } else if (username.equals("")) {
+            } else {
+                String[] splited = username.split(" ");
+                if (splited[0].equals("Teacher")) { //TEACHER IS LOGGED IN
+                    Teacher teacher = new Teacher(splited[1]);
+                    teacher.createMenuGUI(socket);
+                    i++;
+                } else if (splited[0].equals("Student")) { //STUDENT IS LOGGED IN
+                    Student student = new Student(splited[1], socket);
+                    student.mainStudent(socket);
+                    i++;
+                }
+            }
 
 
-            } catch (Exception e) {
-                e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
 
         }
     }
